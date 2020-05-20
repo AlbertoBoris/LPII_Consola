d4e95d5c4789d4fbd9ac474a007ab46229a29954
AlbertoOmonte <Alberto.Omonte@hotmail.com>
2020-05-20 15:42:23 -0500

    Mi primer commit

---
+++ src/entidad/Alumno.java
@@ -0,0 +1,54 @@
+package entidad;
+
+import lombok.Data;
+
+@Data
+public class Alumno {
+
+	private int idAlumno;
+	private String nombre;
+	private String apellido;
+	private String fecha;
+	private String correo;
+
+	public int getIdAlumno() {
+		return idAlumno;
+	}
+
+	public void setIdAlumno(int idAlumno) {
+		this.idAlumno = idAlumno;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+}
